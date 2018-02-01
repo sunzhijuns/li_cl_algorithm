82eaede4c65fce1c7cf3c9c5a44312a3769d2fff
szj_ubuntu_mach <2843359242@qq.com>
2018-02-01 08:47:11 +0800

    SelectionSort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
+void SelectionSort(int arr[], int n){
+    for (int i = 0; i < n; ++i) {
+        int min_index = i;
+        for (int j = i+1; j < n; ++j) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        swap(arr[i], arr[min_index]);
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a[10] = {1,3,3,44};
+    SelectionSort(a,10);
+    for (int i = 0; i < 10; ++i) {
+        cout<<a[i]<<"-";
+    }
+    cout << endl;
     return 0;
 }
