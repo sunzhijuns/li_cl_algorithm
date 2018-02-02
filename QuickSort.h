9cc3fd50c0051d8f463ac573a5635d9bcabfc6ab
szj_ubuntu_mach <2843359242@qq.com>
2018-02-02 12:13:49 +0800

    三路快速排序

--- QuickSort.h
+++ QuickSort.h
@@ -83,4 +83,56 @@ void QuickSort(T arr[], int n){
 
 }
 
+template<typename T>
+void __Partition_3(T arr[], int partition_indies[2], int left, int right){
+
+    std::swap(arr[rand()%(right - left + 1) + left], arr[left]);
+
+    T partition = arr[left];
+
+    int less_index = left + 1;  // 待检查的
+    int great_index = right;    // 待检查的
+    int i = left + 1;
+    while (i <= great_index) {
+        if(arr[i] > partition){
+            std::swap(arr[i], arr[great_index]);
+            great_index--;
+        }else if(arr[i] < partition){
+            std::swap(arr[less_index], arr[i]);
+            less_index ++;
+            i++;
+        }else{
+            i++;
+        }
+    }
+    std::swap(arr[left], arr[less_index-1]);
+
+    partition_indies[0] = less_index-2;
+    partition_indies[1] = great_index + 1;
+
+}
+
+template<typename T>
+void __QuickSort3Ways(T arr[], int left, int right){
+
+    if(right - left <= 15){
+        InsertionSort(arr, left, right);
+        return;
+    }
+
+    int p[2] = {0};
+
+    __Partition_3(arr,p,left,right);
+
+    __QuickSort3Ways(arr,left,p[0]);
+    __QuickSort3Ways(arr,p[1],right);
+
+}
+
+template<typename T>
+void QuickSort3Ways(T arr[], int n){
+    srand(time(NULL));
+    __QuickSort3Ways(arr,0,n-1);
+}
+
 #endif //SUANFA_QUICKSORT_H
--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 
 int main11(){
     int n = 100;
-    int * a = SortTestHelper::GenerateRandomArray(n,0,1);
-    QuickSort(a,n);
+    int * a = SortTestHelper::GenerateRandomArray(n,0,10);
+    QuickSort3Ways(a,n);
     SortTestHelper::PrintArray(a,100);
     delete[] a;
     return 0;
@@ -20,10 +20,12 @@ int main11(){
 int main() {
     int n = 100*10000;
     int print_n = 100;
-    int * arr = SortTestHelper::GenerateRandomArray(n,0,10);
-//    int * arr = SortTestHelper::GenerateNearlyOrderedArray(n,10);
+//    int * arr = SortTestHelper::GenerateRandomArray(n,0,n);
+    int * arr = SortTestHelper::GenerateNearlyOrderedArray(n,1000);
     int * arr1 = SortTestHelper::CopyIntArray(arr,n);
     int * arr2 = SortTestHelper::CopyIntArray(arr,n);
+    int * arr3 = SortTestHelper::CopyIntArray(arr,n);
+
 
 //    SortTestHelper::PrintArray(arr,print_n);
     SortTestHelper::PrintArray(arr1,print_n);
@@ -35,12 +37,17 @@ int main() {
     SortTestHelper::TestSort("QuickSort", QuickSort, arr1, n);
     SortTestHelper::PrintArray(arr1,print_n);
 
-    SortTestHelper::TestSort("MergeSortBottomToUpOptimized", MergeSortBottomToUpOptimized, arr2, n);
+
+    SortTestHelper::TestSort("QuickSort3Ways", QuickSort3Ways, arr2, n);
     SortTestHelper::PrintArray(arr2,print_n);
+
+    SortTestHelper::TestSort("MergeSortBottomToUpOptimized", MergeSortBottomToUpOptimized, arr3, n);
+    SortTestHelper::PrintArray(arr3,print_n);
 
     delete[] arr;
     delete[] arr1;
     delete[] arr2;
+    delete[] arr3;
 
     return 0;
 }
